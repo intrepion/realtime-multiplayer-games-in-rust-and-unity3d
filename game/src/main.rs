478c8274b5fde0b58935e083042755f6f47b0ee7
Oliver Forral <intrepion@gmail.com>
2022-02-15 20:44:27 -0700

    The Game Data Type

--- game/src/main.rs
+++ game/src/main.rs
@@ -2,7 +2,31 @@ use macroquad::prelude::*;
 
 #[macroquad::main("game")]
 async fn main() {
+    let mut game = Game::new();
     loop {
+        game.update();
+        game.draw();
+        if game.quit {
+            return;
+        }
         next_frame().await
+    }
+}
+
+pub struct Game {
+    pub quit: bool,
+}
+ 
+impl Game {
+    pub fn new() -> Self {
+        Self { quit: false }
+    }
+    pub fn update(&mut self) {
+        if is_key_down(KeyCode::Escape) {
+            self.quit = true;
+        }
+    }
+    pub fn draw(&self) {
+        clear_background(color_u8!(0, 211, 205, 205));
     }
 }
