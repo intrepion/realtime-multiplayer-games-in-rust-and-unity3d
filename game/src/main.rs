1c74275b3b7790174bfe7de42d00842ceb04c69d
Oliver Forral <intrepion@gmail.com>
2022-02-15 22:05:15 -0700

    Added plane image

--- game/src/main.rs
+++ game/src/main.rs
@@ -2,7 +2,7 @@ use macroquad::prelude::*;
 
 #[macroquad::main("game")]
 async fn main() {
-    let mut game = Game::new();
+    let mut game = Game::new().await;
     loop {
         game.update();
         game.draw();
@@ -16,22 +16,51 @@ async fn main() {
 pub struct Game {
     pub quit: bool,
     pub player_state: PlayerState,
+    pub texture: Texture2D,
 }
 
 impl Game {
-    pub fn new() -> Self {
+    pub async fn new() -> Self {
+        let texture = load_texture("assets/plane.png").await.unwrap();
+
         Self {
             quit: false,
             player_state: PlayerState {
                 position: Vec2::new(100f32, 100f32),
                 rotation: 0f32,
             },
+            texture,
         }
     }
 
     pub fn update(&mut self) {
         if is_key_down(KeyCode::Escape) {
             self.quit = true;
+        }
+
+        const ROT_SPEED: f32 = 0.015;
+
+        if is_key_down(KeyCode::Right) {
+            self.player_state.rotation += ROT_SPEED;
+        }
+        if is_key_down(KeyCode::Left) {
+            self.player_state.rotation -= ROT_SPEED;
+        }
+
+        const SPEED: f32 = 0.6;
+
+        self.player_state.position += vec2_from_angle(self.player_state.rotation) * SPEED;
+
+        if self.player_state.position.x > screen_width() {
+            self.player_state.position.x = -self.texture.width();
+        } else if self.player_state.position.x < -self.texture.width() {
+            self.player_state.position.x = screen_width();
+        }
+
+        if self.player_state.position.y > screen_height() {
+            self.player_state.position.y = -self.texture.height();
+        } else if self.player_state.position.y < -self.texture.height() {
+            self.player_state.position.y = screen_height();
         }
     }
 
@@ -49,6 +78,17 @@ impl Game {
         );
 
         draw_box(Vec2::new(200f32, 200f32), Vec2::new(10f32, 10f32));
+
+        draw_texture_ex(
+            self.texture,
+            self.player_state.position.x,
+            self.player_state.position.y,
+            WHITE,
+            DrawTextureParams {
+                rotation: self.player_state.rotation,
+                ..Default::default()
+            },
+        );
     }
 }
 
@@ -62,4 +102,9 @@ fn draw_box(pos: Vec2, size: Vec2) {
     let upper_left = pos - size;
 
     draw_rectangle(upper_left.x, upper_left.y, dimension.x, dimension.y, BLACK);
+}
+
+pub fn vec2_from_angle(angle: f32) -> Vec2 {
+    let angle = angle - std::f32::consts::FRAC_PI_2;
+    Vec2::new(angle.cos(), angle.sin())
 }
