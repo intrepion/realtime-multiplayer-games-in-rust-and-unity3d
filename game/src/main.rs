888821e7ec0cb34c2776909b71dafe87136e34bd
Oliver Forral <intrepion@gmail.com>
2022-02-16 02:07:45 -0700

    Receiving messages in the game loop

--- game/src/main.rs
+++ game/src/main.rs
@@ -1,16 +1,28 @@
 use macroquad::prelude::*;
+use ws::Connection;
 
 mod ws;
 
 #[macroquad::main("game")]
 async fn main() {
+    let mut connection = Connection::new();
+    connection.connect("ws://localhost:3030/game");
+
     let mut game = Game::new().await;
+
     loop {
+        if let Some(msg) = connection.poll() {
+            println!("Message received! {:?}", msg);
+        }
+
         game.update();
+
         game.draw();
+
         if game.quit {
             return;
         }
+
         next_frame().await
     }
 }
