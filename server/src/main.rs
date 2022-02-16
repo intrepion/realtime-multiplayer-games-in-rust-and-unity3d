698f338c34d72ee17e3f08f3522fff0d103511f1
Oliver Forral <intrepion@gmail.com>
2022-02-15 22:43:03 -0700

    Setup and Connection Handling

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init();
+
+    let status = warp::path!("status").map(move || warp::reply::html("hello"));
+
+    warp::serve(status).run(([0, 0, 0, 0], 3030)).await;
 }
